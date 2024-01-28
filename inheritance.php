b81e6f0b9bfce13c459af59e8ed4004de7804b68
Prashon <prashon109939@gmail.com>
2024-01-28 09:58:30 +0545

    trait

--- inheritance.php
+++ inheritance.php
@@ -1,5 +1,22 @@
 <?php
 
-class Laptop{
-    
+trait BCA
+{
+    public $x = 10;
 }
+
+class Laptop
+{
+    public function brand()
+    {
+        return 'Laptop';
+    }
+}
+
+class Dell extends Laptop
+{
+    use BCA; //using the traint thing here!!!
+}
+
+$obj = new Dell();
+echo $obj->brand();
